f5e3933672587d353cf15a8e825401e7af468fd1
Christopher James Leo <aungaungoo.hotmail@gmail.com>
2022-12-15 11:52:17 +0630

    add Note

--- upload.php
+++ upload.php
@@ -5,10 +5,15 @@
     $targetFilePath = $pathDir . $filename;
     $fileType = pathinfo($targetFilePath , PATHINFO_EXTENSION); // to find file type;
     include "config.php";
-    move_uploaded_file($filetmp, $targetFilePath);
-    $sql = "INSERT INTO images (file_name, uploaded_on) VALUES ('{$filename}', NOW())";
-    $result = mysqli_query( $conn , $sql);
-    
+    // move_uploaded_file($filetmp, $targetFilePath);
+    // $sql = "INSERT INTO images (file_name, uploaded_on) VALUES ('{$filename}', NOW())";
+    // $result = mysqli_query( $conn , $sql);
+    if(!empty($filename)){ //file name ထဲတွင် empty မဖြစ်နေလျှင် အောက်က code ကို run ပါ
+        if(move_uploaded_file($filetmp, $targetFilePath)){ // image file သည် ပတ်လမ်းကြောင်းထဲသို့ ရောက်သွားမှသာ database ထဲသို့ ပို့ပေးပါ
+            $sql = "INSERT INTO images (file_name, uploaded_on) VALUES ('{$filename}', NOW())"; // အခုလက်ရှိအချိန်ဖြင့် ဖိုင်အား data base ထဲတွင်သိမ်းပါ
+            $result = mysqli_query( $conn , $sql);
+        }
+    }
     
     
     // if(!empty($_FILES["filename"]["name"])){
